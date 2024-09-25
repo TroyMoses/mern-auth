7e07d93b01ed69b9cc2cbd4ae36ba8c83d7f80aa
TroyMoses <mugabimoses07@gmail.com>
2024-09-25 18:16:54 +0800

    send verification email to user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -36,7 +36,7 @@ export const signup = async (req, res) => {
 
     generateTokenAndSetCookie(res, user._id);
 
-    sendVerificationEmail(user.email, verificationToken);
+    await sendVerificationEmail(user.email, verificationToken);
 
     res.status(201).json({
       success: true,
