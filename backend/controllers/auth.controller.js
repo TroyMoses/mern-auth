4684f3ec27e2eb9fe422ad5cf6eeb031d2e83ea1
TroyMoses <mugabimoses07@gmail.com>
2024-11-15 13:28:19 +0300

    password reset success email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 import {
   sendPasswordResetEmail,
+  sendPasswordResetSuccessEmail,
   sendVerificationEmail,
   sendWelcomeEmail,
 } from "../mailtrap/emails.js";
@@ -162,6 +163,42 @@ export const forgotPassword = async (req, res) => {
       message: "Password reset link sent to your email",
     });
 
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid or expired reset token" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTokenExpiresAt = undefined;
+
+    await user.save();
+
+    await sendPasswordResetSuccessEmail(user.email);
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset successfully",
+    });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -83,3 +83,20 @@ export const sendResetSuccessEmail = async (email) => {
 		throw new Error(`Error sending password reset success email: ${error}`);
 	}
 };
+
+export const sendPasswordResetSuccessEmail = async (email) => {
+	const recipient = [{ email }];
+
+	try {
+		const response = await mailtrapClient.testing.send({
+			from: sender,
+			to: recipient,
+			subject: "Password Reset Successful",
+			html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+			category: "Password Reset",
+		});
+	} catch (error) {
+		throw new Error(`Error sending password reset success email: ${error}`);
+	}
+};
+
